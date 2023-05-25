044a9f3396732852626860624303b26f971b0880
TheForgotten0ne <justine.bali123@gmail.com>
2023-05-26 02:34:02 +0800

    Update server.php

--- server/server.php
+++ server/server.php
@@ -1,27 +1,4 @@
 <?php
-// $database	= 'bis';
-// $username	= 'root';
-// $host		= 'localhost';
-// $password	= '';
-
-// ini_set('display_errors',1);
-// error_reporting(E_ALL);
-// mysqli_report(MYSQLI_REPORT_ERROR | E_DEPRECATED | E_STRICT);
-// // error_reporting(0);
-
-// $conn = new mysqli($host,$username,$password,$database);
-
-// if($conn->connect_error){
-// 	die("Connection Failed: ". $conn->connect_error());
-// }
-
-// if(!isset($_SESSION)){
-// 	session_start();	
-// }
-
-// if(!isset($_SESSION['username'])){
-// 	header('Location: login.php');
-// }
 $database_url = getenv("postgres://gdsrtohjbzmqjh:e804ba4e66bdf8317b2597c3cb8fcc7a2b50754e28c6a16a2d32ff2b2d1c2711@ec2-3-214-103-146.compute-1.amazonaws.com:5432/db7ln932kq5cbf");
 $db_params = parse_url($database_url);
 
