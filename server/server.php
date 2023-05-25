f6d579a295e417248bea80704d0d0722510000b3
TheForgotten0ne <justine.bali123@gmail.com>
2023-05-26 01:33:58 +0800

    Update server.php

--- server/server.php
+++ server/server.php
@@ -1,8 +1,8 @@
 <?php
-$database	= 'bis';
-$username	= 'root';
-$host		= 'localhost';
-$password	= '';
+$database	= 'db7ln932kq5cbf';
+$username	= 'gdsrtohjbzmqjh';
+$host		= 'ec2-3-214-103-146.compute-1.amazonaws.com';
+$password	= 'e804ba4e66bdf8317b2597c3cb8fcc7a2b50754e28c6a16a2d32ff2b2d1c2711';
 
 ini_set('display_errors',1);
 error_reporting(E_ALL);
