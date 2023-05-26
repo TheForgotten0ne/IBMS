91178f698358d516f20224d4b8e2af4ae47024f2
TheForgotten0ne <justine.bali123@gmail.com>
2023-05-26 15:44:13 +0800

    please work

--- server/server.php
+++ server/server.php
@@ -1,15 +1,19 @@
 <?php
-$database	= 'bis';
-$username	= 'root';
-$host		= 'localhost';
-$password	= '';
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+
 
 ini_set('display_errors',1);
 error_reporting(E_ALL);
 mysqli_report(MYSQLI_REPORT_ERROR | E_DEPRECATED | E_STRICT);
 // error_reporting(0);
 
-$conn = new mysqli($host,$username,$password,$database);
+$conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 if($conn->connect_error){
 	die("Connection Failed: ". $conn->connect_error());
